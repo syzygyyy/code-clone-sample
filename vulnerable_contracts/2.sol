5a15334761a6e93e2e60b07fdc56481868aceb67
dave.zhou <dave@metatrust.io>
2023-01-06 11:31:52 +0800

    add sample

---
+++ vulnerable_contracts/2.sol
@@ -0,0 +1,9 @@
+    /// @notice Create `mintedAmount` tokens and send it to `target`
+    /// @param target Address to receive the tokens
+    /// @param mintedAmount the amount of tokens it will receive
+    function mintToken(address target, uint256 mintedAmount) onlyOwner public {
+        balanceOf[target] += mintedAmount;
+        totalSupply += mintedAmount;
+        Transfer(0, this, mintedAmount);
+        Transfer(this, target, mintedAmount);
+    }
