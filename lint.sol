7f1aa9a823af74d5623745b4e2be8df2e7c688ee
syzygyyy <dave@metatrust.io>
2023-04-10 12:35:41 +0800

    Create lint.sol

---
+++ lint.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.0;
+contract MyConc{
+    using SafeMath for uint;   
+    uint a = 0x3f5b3f7c4ce4a1cf720840d4a4e4b04a2aa2a8a1;
+    function my_func(uint a, uint b) public returns (bool){
+        require()  ; 
+        sha3(b);
+        return aa;
+    }
+    function func(uint a, uint b) public{
+        if (a > b) {
+            a = a + b;
+        }
+        require();
+        my_func(b);
+        a=+1;
+        uint []x;
+        suicide(q);
+    }
+    function foo() myModifier(100, \"hello\") constant boo;
+}
